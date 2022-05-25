1750f1c488bd9f6913176f38397cb73dd208117b
Darvan42 <darvan42@mailbox.org>
2022-05-25 11:31:01 +0200

    Change exported function to not include any text
    The node environment where the script is run does not include TextDecoder or any other utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 
 #[wasm_bindgen]
-pub fn hello_screeps() {
-    console::log_1(&JsValue::from("Hello from the other side! (Rust is here)"))
+pub fn add_two(x: isize) -> isize {
+    x + 2
 }
